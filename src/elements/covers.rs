7db509e1793be1a297598c834bc346080bb0ffef
Orangethewell <orangethewell@gmail.com>
2022-02-17 19:15:50 -0300

    Fix cover rendering for multiple aspect radios

--- src/elements/covers.rs
+++ src/elements/covers.rs
@@ -39,29 +39,57 @@ impl Cover {
     
     pub fn draw(&self, d: &mut RaylibDrawHandle, screen_size: Vector2){
         let img_rec = Rectangle::new(0.0, 0.0, self.img.width as f32, self.img.height as f32);
+
         if img_rec.width == img_rec.height {
+            let mut dest_size = screen_size.x;
+            if screen_size.y < dest_size {
+                dest_size = screen_size.y
+            }
+
+            dest_size -= 80.0;
+            let dest_rectangle = Rectangle::new(
+                screen_size.x / 2.0, 
+                screen_size.y / 2.0 + 40.0, 
+                dest_size, 
+                dest_size
+            );
+            let origin = Vector2::new(dest_rectangle.width / 2.0, dest_rectangle.height / 2.0);
             d.draw_texture_pro(
                 &self.img, 
                 img_rec, 
-                Rectangle::new(
-                    self.pos.x + screen_size.x / 2.0, 
-                    self.pos.y, 
-                    self.size.x - screen_size.x / 2.0, 
-                    self.size.y
-                ), 
-                self.origin, 
-                self.rotation + 32.0, 
+                dest_rectangle, 
+                origin, 
+                self.rotation, 
+                Color::WHITE
+            )
+        } else {
+            let mut width_scale = screen_size.x / img_rec.width;
+            let mut height_scale: f32;
+            if img_rec.height * width_scale > screen_size.y {
+                height_scale = screen_size.y / img_rec.height;
+            } else {
+                height_scale = width_scale;
+            }
+
+            width_scale -= 0.016;
+            height_scale -= 0.05;
+
+            let dest_rectangle = Rectangle::new(
+                screen_size.x / 2.0, 
+                screen_size.y / 2.0 + 40.0, 
+                width_scale * img_rec.width,
+                height_scale * img_rec.height
+            );
+            let origin = Vector2::new(dest_rectangle.width / 2.0, dest_rectangle.height / 2.0);
+            d.draw_texture_pro(
+                &self.img, 
+                img_rec, 
+                dest_rectangle, 
+                origin, 
+                self.rotation, 
                 Color::WHITE
             )
         }
-        d.draw_texture_pro(
-            &self.img, 
-            img_rec, 
-            Rectangle::new(self.pos.x, self.pos.y, self.size.x, self.size.y), 
-            self.origin, 
-            self.rotation, 
-            Color::WHITE
-        )
     }
 
     pub fn draw_lines(&self, d: &mut RaylibDrawHandle){
